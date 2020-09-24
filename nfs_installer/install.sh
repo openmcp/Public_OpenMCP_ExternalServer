0d91b98d532f0c025e4ae54040abed0a40eb704d
chkim <atyx300@naver.com>
2020-09-24 10:54:34 +0900

    nfs influxdb 디렉토리 생성

--- nfs_installer/install.sh
+++ nfs_installer/install.sh
@@ -7,7 +7,7 @@ chmod 777 /home/nfs
 
 ssh-keygen -f /root/.ssh/id_rsa -t rsa -P ""
 mkdir /home/nfs/ssh && cp /root/.ssh/id_rsa.pub /home/nfs/ssh
-mkdir /home/nfs/pv
+mkdir -p /home/nfs/pv/influxdb
 mkdir /home/nfs/openmcp
 
 echo "/home/nfs *(rw,no_root_squash,sync)" >> /etc/exports
